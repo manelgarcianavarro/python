2eed9f33526d7723e2c46f4cfa80c798cf5d3e86
unknown <2btd07@IESRAMONLLULL.LOCAL>
2023-01-26 13:06:07 +0100

    segundo grado

--- enteros.py
+++ enteros.py
@@ -1,4 +1,10 @@
+import math
+print("¿Cual es el a?")
 a = input()
+print("¿Cual es el b?")
 b = input()
+print("¿Cual es el c?")
 c = input()
-print((-b pow(b -4**a**b)/2**a))
+d = (-b * math.sqrt(pow(b,2) -4*a*b)/2*a)
+
+print(d)
